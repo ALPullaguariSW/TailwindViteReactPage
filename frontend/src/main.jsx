0c7d4322f59c8dadf85f9336229a14523fca413d
Axel Lenin Pullaguari Cedeño <150882583+ALPullaguariSW@users.noreply.github.com>
2024-02-11 20:29:57 -0500

    App Tailwind

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import About from "./components/About.jsx";
+import Base from "./components/Base.jsx";
+import Footer from "./components/Footer.jsx";
+import FirstSection from "./components/First Section.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Base />
+    <FirstSection />
+    <About />
+    <Footer />
+    
+  </React.StrictMode>
+);
